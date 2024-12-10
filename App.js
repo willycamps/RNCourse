139f5423efd0ac1076102aac27882b35b61ee50f
root <root@gitlab.jaguar-labs.io>
2024-12-10 19:03:28 +0100

    Run App in local machine

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
+
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
 const Stack = createNativeStackNavigator();
